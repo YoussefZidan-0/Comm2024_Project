492d7cfce66374e580d284c6a2bdf56dd1a71522
YoussefZidan-0 <youssefzidan.eng@gmail.com>
2024-11-30 20:29:18 +0200

    final touches edited the report and orgazined files

--- Modulation.m
+++ Modulation.m
@@ -1,40 +1,35 @@
-<<<<<<< HEAD
-fc_mod = zeros(1,5);  % Carrier frequencies
-t = cell(1, 5);  % Time vectors for each signal
+% Constants
+fc_mod = zeros(1, 5);  % Carrier frequencies
 modulated_signal = cell(1, 5);  % Modulated signals
-sentsignal=[];
-max_length=0;
-interp_factor = 14;
-fs=44100;
-fs_interpolated=interp_factor*fs;
+sentsignal = [];  % Combined FDM signal
+interp_factor = 14;  % Interpolation factor
+fs = 44100;  % Sampling frequency
+fs_interpolated = interp_factor * fs;  % Interpolated sampling frequency
+
+% Modulation process
 for i = 1:length(audioSignals)
-    % Retrieve the current mono signal
+    % Retrieve the padded mono signal
     x = paddedSignals{i};
 
-    % Take quarter of the length of the signal
-    half_length = floor(length(x) / 1);
-    x = x(1:half_length);  % Use the first quarter of the signal
+    % Take the first portion of the signal
+    x = x(1:floor(length(x) / 1));  
 
     % Carrier frequency (in Hz)
-    fc_mod(i) = 100e3 + (i - 1) * 50e3;  % Carrier frequency for the i-th signal
+    fc_mod(i) = 100e3 + (i - 1) * 50e3;
 
-
-
-        x_inter=interp(x,interp_factor);
-                 t = (0:1/fs_interpolated:((size(x_inter, 1)-1)/fs_interpolated))';
-                 t = t(:, ones(1, size(x_inter, 2)));
-        Mod_carrier = cos(2 * pi * fc_mod(i) * t);  
-        modulated_signal{i} = x_inter .* Mod_carrier;  
-        
-        %modulated_signal{i}=ammod(x_inter,fc_mod(i),fs*interp_factor);
-           %     t_interpolated = (0:length(x_inter)-1) / (interp_factor * fs);  % New time vector
-
-        max_length = max(max_length, length(modulated_signal{i}));
-
+    % Interpolate the signal
+    x_inter = interp(x, interp_factor);
+    
+    % Time vector for the interpolated signal
+    t = (0:1/fs_interpolated:(length(x_inter) - 1) / fs_interpolated)';
+    
+    % Generate the carrier signal and perform modulation
+    Mod_carrier = cos(2 * pi * fc_mod(i) * t);
+    modulated_signal{i} = x_inter .* Mod_carrier;
 end
 
+% Combine all modulated signals into the FDM signal
 for i = 1:length(modulated_signal)
-    % Combine into the FDM signal
     if isempty(sentsignal)
         sentsignal = modulated_signal{i};
     else
@@ -45,7 +40,7 @@ end
 % Plot the combined FDM signal in the time domain
 figure;
 subplot(2, 1, 1);
-t_total = (0:length(sentsignal)-1) / (10*fs);  % Time vector for the combined signal
+t_total = (0:length(sentsignal) - 1) / fs_interpolated;  % Time vector for FDM signal
 plot(t_total, sentsignal);
 title('FDM Signal (Time Domain)');
 xlabel('Time (s)');
@@ -53,75 +48,14 @@ ylabel('Amplitude');
 
 % Plot the combined FDM signal in the frequency domain
 subplot(2, 1, 2);
-N = length(sentsignal);  % Number of samples in the combined signal
-f = (-N/2:N/2-1) * 10*fs / N;  % Frequency axis
+N = length(sentsignal);
+f = (-N/2:N/2-1) * fs_interpolated / N;  % Frequency axis
 spectrum_fdm = fftshift(fft(sentsignal, N));  % Compute and shift the FFT
 plot(f, abs(spectrum_fdm) / N);
 title('FDM Signal (Frequency Domain)');
 xlabel('Frequency (Hz)');
 ylabel('Magnitude');
-%sound(sentsignal, 10 * fs);
-=======
-fc_mod = zeros(1,5);  % Carrier frequencies
-t = cell(1, 5);  % Time vectors for each signal
-modulated_signal = cell(1, 5);  % Modulated signals
-sentsignal=[];
-max_length=0;
-interp_factor = 14;
-fs=44100;
-fs_interpolated=interp_factor*fs;
-for i = 1:length(audioSignals)
-    % Retrieve the current mono signal
-    x = paddedSignals{i};
 
-    % Take quarter of the length of the signal
-    half_length = floor(length(x) / 1);
-    x = x(1:half_length);  % Use the first quarter of the signal
-
-    % Carrier frequency (in Hz)
-    fc_mod(i) = 100e3 + (i - 1) * 50e3;  % Carrier frequency for the i-th signal
-
-
-
-        x_inter=interp(x,interp_factor);
-                 t = (0:1/fs_interpolated:((size(x_inter, 1)-1)/fs_interpolated))';
-                 t = t(:, ones(1, size(x_inter, 2)));
-        Mod_carrier = cos(2 * pi * fc_mod(i) * t);  
-        modulated_signal{i} = x_inter .* Mod_carrier;  
-        
-        %modulated_signal{i}=ammod(x_inter,fc_mod(i),fs*interp_factor);
-           %     t_interpolated = (0:length(x_inter)-1) / (interp_factor * fs);  % New time vector
-
-        max_length = max(max_length, length(modulated_signal{i}));
-
-end
-
-for i = 1:length(modulated_signal)
-    % Combine into the FDM signal
-    if isempty(sentsignal)
-        sentsignal = modulated_signal{i};
-    else
-        sentsignal = sentsignal + modulated_signal{i};
-    end
-end
-
-% Plot the combined FDM signal in the time domain
-figure;
-subplot(2, 1, 1);
-t_total = (0:length(sentsignal)-1) / (10*fs);  % Time vector for the combined signal
-plot(t_total, sentsignal);
-title('FDM Signal (Time Domain)');
-xlabel('Time (s)');
-ylabel('Amplitude');
-
-% Plot the combined FDM signal in the frequency domain
-subplot(2, 1, 2);
-N = length(sentsignal);  % Number of samples in the combined signal
-f = (-N/2:N/2-1) * 10*fs / N;  % Frequency axis
-spectrum_fdm = fftshift(fft(sentsignal, N));  % Compute and shift the FFT
-plot(f, abs(spectrum_fdm) / N);
-title('FDM Signal (Frequency Domain)');
-xlabel('Frequency (Hz)');
-ylabel('Magnitude');
-%sound(sentsignal, 10 * fs);
->>>>>>> bdfa72de2de8dc9a03a901e43a42f503bc13dc5a
+% Play the FDM signal
+%sound(sentsignal, fs_interpolated);
+clearvars -except BWs   sentsignal interp_factor fs
--- Samples.m
+++ Samples.m
@@ -1,10 +1,11 @@
-%%section1: load data
+%% Section 1: Load Data
+addpath 'Audio Signals'\
+[c1, fs1] = audioread("Short_BBCArabic2.wav");
+[c2, fs2] = audioread("Short_FM9090.wav");
+[c3, fs3] = audioread("Short_QuranPalestine.wav");
+[c4, fs4] = audioread("Short_RussianVoice.wav");
+[c5, fs5] = audioread("Short_SkyNewsArabia.wav");
 
-[c1,fs1]=audioread("Short_BBCArabic2.wav");
-[c2,fs2]=audioread("Short_FM9090.wav");
-[c3,fs3]=audioread("Short_QuranPalestine.wav");
-[c4,fs4]=audioread("Short_RussianVoice.wav");
-[c5,fs5]=audioread("Short_SkyNewsArabia.wav");
 audioSignals = {c1, c2, c3, c4, c5};
 samplingRates = [fs1, fs2, fs3, fs4, fs5];
 Left_channel = cell(1, length(audioSignals));
@@ -13,53 +14,46 @@ monoSignals = cell(1, 5);
 paddedSignals = cell(1, 5);
 maxLength = 0;
 
-for i=1:length(audioSignals)
-    Left_channel{i}=audioSignals{i}(:,1);
+% Extract left, right, and mono channels; calculate max signal length
+for i = 1:length(audioSignals)
+    Left_channel{i} = audioSignals{i}(:, 1);
     Right_channel{i} = audioSignals{i}(:, 2);
-    monoSignals{i}=sum(audioSignals{i}, 2);
-    maxLength = max(maxLength, length( monoSignals{i}));
-
+    monoSignals{i} = sum(audioSignals{i}, 2);  % Combine left and right channels
+    maxLength = max(maxLength, length(monoSignals{i}));
 end
-% sound(monoSignals{2},fs2)
 
-% Pad signals to the maximum length
+% Pad signals to match the maximum length
 for i = 1:length(monoSignals)
-    x = monoSignals{i};
-    % Pad the signal with zeros to match the maximum length
-    paddedSignals{i} = [x; zeros(maxLength - length(x), 1)];
+    paddedSignals{i} = [monoSignals{i}; zeros(maxLength - length(monoSignals{i}), 1)];
 end
-%% section 2 : plot and extract data
 
-% plotting for the all original signal
-BWs = zeros(1,5);
+%% Section 2: Plot and Extract Data
+
+BWs = zeros(1, 5);  % Initialize array for bandwidths
+
+% Plot frequency spectra and calculate bandwidths
 for i = 1:length(audioSignals)
     x = audioSignals{i};
     fs = samplingRates(i);
 
-    % FFT
+    % FFT for frequency spectrum
     N = length(x);
     X = fft(x, N);
     f = (-N/2:N/2-1) * fs / N;
 
-    %[pxx, f_pxx] = periodogram(x, [], length(x), fs);
-    %  avg_pxx = mean(pxx, 2);
+    % Calculate bandwidth
+    BWs(i) = obw(monoSignals{i}, fs);
 
-    %     pxx=abs(x).^2;
-    %     tot_sum=cumsum(pxx);
-    %     normlizedsum=tot_sum/tot_sum(end);
-
-    BWs(i)=obw(monoSignals{i},fs);
-
-    % Plot
-    grid on
+    % Plot frequency spectrum
     figure;
     plot(f, abs(fftshift(X)) / N);
     title(['Frequency Spectrum of Signal ', num2str(i)]);
     xlabel('Frequency (Hz)');
     ylabel('Magnitude');
+    grid on;
 end
-grid off
 
-
-BWs_Hz = BWs ;
+% Convert bandwidths to Hz and kHz
+BWs_Hz = BWs;
 BWs_kHz = BWs_Hz / 1000;
+clearvars -except BWs paddedSignals audioSignals
